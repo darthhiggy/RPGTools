02f0b280a9f19615111ee3dcaad5f3ab6d519200
Michael <darthhiggy@users.noreply.github.com>
2018-09-18 23:04:57 -0400

    Fixed Die side property and implement Die unit test

--- SWRPG/SWRPG/Dice/Die.cs
+++ SWRPG/SWRPG/Dice/Die.cs
@@ -13,6 +13,38 @@ namespace Dice
         {
             get
             {
+                if(_sides <= 0)
+                {
+                    switch (Type)
+                    {
+                        case DiceType.Ability:
+                            _sides = 8;
+                            break;
+                        case DiceType.Proficiency:
+                            _sides = 12;
+                            break;
+                        case DiceType.Boost:
+                            _sides = 6;
+                            break;
+                        case DiceType.Difficulty:
+                            _sides = 8;
+                            break;
+                        case DiceType.Challenge:
+                            _sides = 12;
+                            break;
+                        case DiceType.Setback:
+                            _sides = 6;
+                            break;
+                        case DiceType.Force:
+                            _sides = 12;
+                            break;
+                        case DiceType.TenSided:
+                            _sides = 10;
+                            break;
+                        default:
+                            break;
+                    }
+                }
                 return _sides;
             }
 
@@ -54,7 +86,7 @@ namespace Dice
         public Die(DiceType type)
         {
             this.Type = type;
-
+            
         }
 
         public int Roll()
--- SWRPG/SWRPG/DiceTest/DieTest.cs
+++ SWRPG/SWRPG/DiceTest/DieTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Dice;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DiceTest
@@ -9,7 +10,35 @@ namespace DiceTest
         [TestMethod]
         public void SidesTest()
         {
+            // Declare 
+            var ability = new Die(DiceType.Ability);
+            var boost = new Die(DiceType.Boost);
+            var challenge = new Die(DiceType.Challenge);
+            var difficulty = new Die(DiceType.Difficulty);
+            var force = new Die(DiceType.Force);
+            var proficiency = new Die(DiceType.Proficiency);
+            var setback = new Die(DiceType.Setback);
+            var tensided = new Die(DiceType.TenSided);
 
+            // Setup
+            var ablSides = 8;
+            var boostSides = 6;
+            var chlgSides = 12;
+            var diffSides = 8;
+            var forceSides = 12;
+            var profSides = 12;
+            var setbackSides = 6;
+            var percentSides = 10;
+
+            // Assert
+            Assert.AreEqual(ablSides, ability.Sides);
+            Assert.AreEqual(boostSides, boost.Sides);
+            Assert.AreEqual(chlgSides, challenge.Sides);
+            Assert.AreEqual(diffSides, difficulty.Sides);
+            Assert.AreEqual(forceSides, force.Sides);
+            Assert.AreEqual(profSides, proficiency.Sides);
+            Assert.AreEqual(setbackSides, setback.Sides);
+            Assert.AreEqual(percentSides, tensided.Sides);
         }
     }
 }
