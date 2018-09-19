b130f72b0f8df1cf3240bb819cf324582bcb895c
Michael <m_higginbotham@live.com>
2018-09-18 22:35:05 -0400

    dice unit test

--- SWRPG/SWRPG/DiceTest/DicePoolTest.cs
+++ SWRPG/SWRPG/DiceTest/DicePoolTest.cs
@@ -33,6 +33,32 @@ namespace DiceTest
 
             // Assert
             Assert.AreEqual(8, result.Count);
+            Assert.AreEqual(7, pool.Pool.Count);
+        }
+
+        [TestMethod]
+        public void BoostDieTest()
+        {
+            // Declare
+            var pool = new DicePool();
+            pool.AddDie(DiceType.Boost);
+
+            // Setup
+            var results = pool.Roll();
+
+            foreach(var res in results.Keys)
+            {
+                Console.WriteLine($"{res} : {results[res]}");
+            }
+
+            // Assert
+            Assert.IsTrue(results[ResultTypes.DarkPoint] == 0);
+            Assert.IsTrue(results[ResultTypes.Dispair] == 0);
+            Assert.IsTrue(results[ResultTypes.Failure] == 0);
+            Assert.IsTrue(results[ResultTypes.LightPoint] == 0);
+            Assert.IsTrue(results[ResultTypes.Threat] == 0);
+            Assert.IsTrue(results[ResultTypes.Triumph] == 0);
+
         }
     }
 }
---
+++ SWRPG/SWRPG/DiceTest/DieTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiceTest
+{
+    [TestClass]
+    public class DieTest
+    {
+        [TestMethod]
+        public void SidesTest()
+        {
+
+        }
+    }
+}
